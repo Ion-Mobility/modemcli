b5d98a70548607e0a889305c66cc7eccffe2f208
Saydi <truong@ionmobility.com>
2024-07-03 16:13:28 +0700

    update to add first method for modemcli

--- src/libs/modem_cli.rs
+++ src/libs/modem_cli.rs
@@ -12,14 +12,16 @@ pub struct IonModemCli {
     destination: String,
     object: String,
     modem: String,
+    ready: bool,
 }
 
 impl IonModemCli {
-    pub fn new(destination: String, object: String, modem: String) -> Self {
+    pub fn new(destination: String, object: String, modem: String, ready: bool) -> Self {
         IonModemCli {
             destination,
             object,
-            modem
+            modem,
+            ready
         }
     }
 
@@ -27,27 +29,74 @@ impl IonModemCli {
         IonModemCli {
             destination: "org.freedesktop.ModemManager1".to_owned(),
             object: "/org/freedesktop/ModemManager1".to_owned(),
-            modem: "/org/freedesktop/ModemManager1/Modem/0".to_owned()
+            modem: String::new(),
+            ready: false
         }
     }
 
+    fn modem_preparing(&mut self) -> bool {
+        // Placeholder implementation
+        let _modempath = self.modem_path_detection();
+        if !_modempath.is_empty() {
+            println!("Just found an modem available, so update itself");
+            self.modem = _modempath;
+            return true;
+        }
+        false
+    }
+
     pub fn is_ready(&self) -> bool {
-        // Placeholder implementation
-        false
+        self.ready
     }
 
-    pub fn is_modem_enable(&self) -> bool {
-        // Placeholder implementation
-        false
+    pub fn waiting_for_ready(&mut self) -> bool {
+        if !self.ready {
+            if self.modem_preparing() {
+                self.ready = true;
+            }
+        }
+        self.ready
     }
 
-    pub fn waiting_for_ready(&self) -> bool {
-        // Placeholder implementation
-        false
-    }
+    fn get_modem_properties(&self, object: &str, prop: &str) -> u32 {
+        // Connect to the system bus
+        let conn = Connection::new_system();
 
-    pub fn get_modem_properties(&self, object: &str, prop: &str) -> u32 {
-        // Placeholder implementation
+        let interface = "org.freedesktop.DBus.Properties";
+
+        // Prepare the D-Bus message to get the Enabled property
+        let msg = Message::new_method_call(&self.destination, &self.modem, interface, "Get")
+            .map_err(|e| {
+                eprintln!("Failed to create method call: {}", e);
+                Error::new_custom("MethodCall", "Failed to create method call")
+            }).expect("REASON")
+            .append2(object, prop);
+
+        // Send the message and await the response
+        let reply = conn.expect("REASON").send_with_reply_and_block(msg, Duration::from_secs(2));
+        println!("{:?}", reply);
+        let enabled_variant = reply.expect("REASON").get1::<MessageItem>();
+        println!("{:?}", enabled_variant);
+
+        match enabled_variant {
+            Some(MessageItem::Variant(dict)) => {
+                match *dict {
+                    MessageItem::UInt32(id) => {
+                        println!("UValue: {}", id);
+                        return id;
+                    }
+                    MessageItem::Int32(id) => {
+                        println!("SValue: {}", id);
+                        return id as u32;
+                    }
+                    MessageItem::Struct(ref id) => {
+                        println!("{:?}", id);
+                    }
+                    _ => {}
+                }
+            }
+            _ => {}
+        }
         0
     }
 
@@ -56,7 +105,7 @@ impl IonModemCli {
         false
     }
 
-    pub fn modem_path_detection(&self) -> String {
+    fn modem_path_detection(&self) -> String {
         // Initialize modempath as an empty string
         let mut modempath: String = String::new();
         
@@ -78,4 +127,59 @@ impl IonModemCli {
         
         modempath
     }
+
+    pub fn is_location_enabled(&self) -> bool {
+        return (self.get_modem_properties("org.freedesktop.ModemManager1.Modem.Location", "Enabled") & 4) != 0;
+    }
+    pub fn is_modem_enabled(&self) -> bool {
+        return (self.get_modem_properties("org.freedesktop.ModemManager1.Modem", "State") & 8) != 0;
+    }
+    pub fn get_signal_quality(&self) -> u32 {
+        return self.get_modem_properties("org.freedesktop.ModemManager1.Modem", "SignalQuality");
+    }
+
+    pub fn get_location(&self) -> String {
+        let mut nmea_str: String = String::new();
+        if self.is_location_enabled() {
+            // Connect to the system bus
+            let c = Connection::new_system().expect("D-Bus connection failed");
+        
+            // Specify the interface and method to call for getting location
+            let interface = "org.freedesktop.ModemManager1.Modem.Location";
+        
+            // Prepare the D-Bus message
+            let gpsmethod = "GetLocation";
+        
+            let msg = Message::new_method_call(&self.destination, &self.modem, interface, gpsmethod)
+                .expect("Failed to create method call");
+        
+            // Send the message and await the response
+            let reply = c.send_with_reply_and_block(msg, Duration::from_secs(2));
+        
+            // Parse the response to get the Args
+            let responds: Vec<MessageItem> = reply.expect("REASON").get_items();
+            for respond in responds.iter() {
+                match respond {
+                    MessageItem::Dict(dict) => {
+                        let a = dict.to_vec();
+                        for (x, y) in a {
+                            if let MessageItem::UInt32(id) = x {
+                                if id == 4 {
+                                    if let MessageItem::Variant(var) = y {
+                                        if let MessageItem::Str(nmea) = *var {
+                                            nmea_str = nmea;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    _ => {}
+                }
+            }
+        }
+
+        nmea_str
+    }
+
 }
--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,18 @@ use std::time::Duration;
 use libs::modem_cli::*;
 
 fn main() {
-    let modem_cli = IonModemCli::new_default();
+    let mut modem_cli = IonModemCli::new_default();
 
     println!("Modem CLI: {:?}", modem_cli);
 
     loop {
-        println!("{:?}", modem_cli.modem_path_detection());
+        if modem_cli.waiting_for_ready() {
+            // println!("Location: {}, ModemEnable: {}, SignalQuality: {}", modem_cli.is_location_enabled(), modem_cli.is_modem_enabled(), modem_cli.get_signal_quality());
+            println!("{}", modem_cli.get_location());
+        } else {
+            println!("Modem is not ready");
+        }
+
         thread::sleep(Duration::from_millis(1000));
     }
 }
